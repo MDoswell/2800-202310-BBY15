ec003fd21a323884c0093cf7b572051a78c2c30e
Mike Doswell <mike.doswell@gmail.com>
2023-05-10 14:07:06 -0700

    Add email validation in changePasswordEmailSubmit and show user's security question in changePasswordQuestion.

--- routes/changePasswordEmailSubmit.js
+++ routes/changePasswordEmailSubmit.js
@@ -4,8 +4,9 @@ const { router } = require('../config/dependencies');
 // Route below.
 router.post('/changePassword/begin/submit', (req, res) => {
     // res.render("changePasswordQuestion");
+    const email = req.body.email;
     console.log('submitted email');
-    res.redirect('/changePassword/question');
+    res.redirect('/changePassword/question?email=' + email);
 });
 
 module.exports = router;
