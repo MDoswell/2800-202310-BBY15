27f22df939a88b0c8b22df5e20052dd789d7cd11
Ebod Shojaei <ebod.shojaei@gmail.com>
2023-05-10 20:32:48 -0700

    - Revised login to enable authentication with username or email. - Created middleware function to redirect logged out users to homepage if attempting to access /logout webpage. - Set up alerts on login so that users receive a notification with a custom error message.

--- public/js/loginRedirect.js
+++ public/js/loginRedirect.js
@@ -1,6 +1,7 @@
 // Enable session validation as middleware to use in route handlers.
 const { isValidSession } = require('./sessionValidation');
 
+// Redirect to home if logged out user is trying to access '/login' webpage.
 function loginRedirect(req, res, next) {
     if (isValidSession(req)) {
         console.log('User already logged in. Redirecting to home.');
