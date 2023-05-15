94605779a33b767afd9de343e42adb779c6622e5
Ebod Shojaei <ebod.shojaei@gmail.com>
2023-05-15 15:31:16 -0700

    - Created getRoutine.js function to retrieve user's saved routine on login. If no routine, prompt user to create one.

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,16 @@
 // Load modules below.
 const { router } = require('../config/dependencies');
+const getRoutine = require('../public/js/getRoutine');
 
 // Route below.
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     if (req.session.authenticated) {
+        const username = req.session.name;
+        await getRoutine(username);
         res.render("index_validSession", { name: req.session.name });
+
+
+
     } else {
         res.render("index_invalidSession");
     }
