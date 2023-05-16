96bc60ea61bdd52cdae6d850b558d46f2ce92ab1
Mike Doswell <mike.doswell@gmail.com>
2023-05-16 12:55:34 -0700

    Adds code to send list of exercises in one request to GPT.

--- routes/openaiRoute.js
+++ routes/openaiRoute.js
@@ -5,38 +5,56 @@ const { router } = require('../config/dependencies');
 
 
 // Route below.
-router.get('/openai', async(req, res) => {
-  
-    var prompts = [];
-    prompts.push({ role: 'user', content: 'some prompt' });
+router.get('/openai', async (req, res) => {
 
-    try {
-        // const response = await openai.createCompletion({
-        //   engine: 'text-davinci-003',
-        //   prompt: "give me a book name",
-        //   maxTokens: 10,
-        //   temperature: 0.5,
-        //   n: 2,
-        //   stop: '\n ',
-        // });
-        const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: prompts,
-            max_tokens: 50,
-            temperature: 0,
-          });
-          console.log("response: " + response);
-          const summary = response.data.choices[0].text.trim();
-          console.log(summary);
-    res.render("openaiTest", {response: response});
-    
-    } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: 'Internal server error' });
-          };
+  const { exerciseCollection } = await require('../config/databaseConnection');
+  const exercises = exerciseCollection.find({});
 
-   
-    
+  var exerciseArray = await exercises.toArray();
+
+  // console.log(test[0]);
+
+  // await exercises.forEach(console.log('a'));
+
+  var prompts = [];
+  prompts.push({ role: 'user', content: 'Make an exercise routine suitable for begginers, using only exercises from the following list. Please include the id for each exercise. Here is the list:' });
+
+  // exerciseArray.forEach(exercise => console.log(exercise.name));
+  for (var i = 0; i < 10; i++) {
+    prompts.push({ role: 'user', content: exerciseArray[i].name + ' (id: ' + exerciseArray[i].id + '),'});
+  }
+
+  // console.log(prompts);
+
+  // res.send("Complete");
+
+  try {
+    // const response = await openai.createCompletion({
+    //   engine: 'text-davinci-003',
+    //   prompt: "give me a book name",
+    //   maxTokens: 10,
+    //   temperature: 0.5,
+    //   n: 2,
+    //   stop: '\n ',
+    // });
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: prompts,
+      max_tokens: 4096,
+      temperature: 0.5,
+    });
+    console.log("response: " + response);
+    const summary = response.data.choices[0].message.content;
+    console.log(summary);
+    res.render("openaiTest", { response: summary });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  };
+
+
+
 
 });
 
