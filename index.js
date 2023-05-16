85f01a02a827337e014d33a559f81f5a9dea5e20
Jeffery Lee <jefferywllee@gmail.com>
2023-05-16 09:25:48 -0700

    new routine questinnaire and page created

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ app.use('/setup', require('./routes/firstTimeSetup'));
 app.use('/profileUpdate', require('./routes/profileUpdate'));
 app.use('/timeForm', require('./routes/timeForm'));
 app.use('/timeFrom/submit', require('./routes/timeFormSubmit')); // post
+app.use('/newRoutine', require('./routes/newRoutine'));
 app.use('*', require('./routes/error404'));
 // Once connectDB is resolved by connecting to the MongoDB databases, start the server.
 connectDB.then(() => {
