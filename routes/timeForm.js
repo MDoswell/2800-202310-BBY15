1764037597e18ccf6968903ee910fb7832326416
Jeffery Lee <jefferywllee@gmail.com>
2023-05-23 10:49:29 -0700

    checks if user has a routine if yes, gives user the choice to add new exercises to new availability or old routine to new availability. (only buttons right now)

--- routes/timeForm.js
+++ routes/timeForm.js
@@ -2,15 +2,23 @@
 require('dotenv').config();
 const { router } = require('../config/dependencies');
 const { sessionValidation } = require('../public/js/sessionValidation');
-
+const getRoutine = require('../public/js/getRoutine');
 // Route below.
-router.get('/timeForm', sessionValidation, (req, res) => {
+router.get('/timeForm', sessionValidation, async (req, res) => {
+    const username = req.session.name;
+    const userRoutine = await getRoutine(username);
+
+        if (userRoutine != null) {    
+            var routine = true;
+        } else {
+            var routine = false
+        }
     const { success } = req.query; // Check if 'success' query parameter is present
 
     const showSuccessMessage = success === 'true';
     const showErrorMessage = success === 'false';
 
-    res.render("timeForm", { showSuccessMessage, showErrorMessage }); // Pass showSuccessMessage and showErrorMessage parameters to the template
+    res.render("timeForm", { showSuccessMessage, showErrorMessage, routine }); // Pass showSuccessMessage and showErrorMessage parameters to the template
 });
 
 module.exports = router;
--- routes/timeFormSubmit.js
+++ routes/timeFormSubmit.js
@@ -4,6 +4,11 @@ const { sessionValidation } = require('../public/js/sessionValidation');
 // Server file
 router.post('/timeForm/submit', sessionValidation, async (req, res) => {
   const { userCollection } = await require('../config/databaseConnection');
+  if (req.body.new != null){
+    // add new routine to new avialability
+  } else if (req.body.old != null){
+    //add old routine to new availability
+  }
 
   try {
     const availabilityData = JSON.parse(req.body.availabilityData); // Parse the availabilityData from the request body
