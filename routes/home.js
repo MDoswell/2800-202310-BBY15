c91de394868b6a675b178d42db47f2239bbb1c14
Mike Doswell <mike.doswell@gmail.com>
2023-05-23 18:19:40 -0700

    Adjusts home feed to fill new availabilities with exercises from previous days.

--- routes/home.js
+++ routes/home.js
@@ -1,31 +1,108 @@
 // Load modules below.
 const { router } = require('../config/dependencies');
+// const fillEmptyDays = require('../public/js/fillEmptyDays');
+const getAvailability = require('../public/js/getAvailability');
 const getRoutine = require('../public/js/getRoutine');
 
 // Route below.
 router.get('/', async (req, res) => {
-    const { exerciseCollection } = await require('../config/databaseConnection');
+    const { userCollection, exerciseCollection } = await require('../config/databaseConnection');
     const username = req.session.name;
-    const userRoutine = await getRoutine(username);
+    // const thisUser = userCollection.findOne({ name: username });
+    var userRoutine = await getRoutine(username);
+    const userAvailability = await getAvailability(username);
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     if (req.session.authenticated) {
         if (userRoutine != null) {
-            const uniqueDates = Array.from(new Set(userRoutine.map(exercise => exercise.date))).sort((a, b) => {
+            const uniqueDates = Array.from(new Set(userAvailability.map(exercise => exercise.date))).sort((a, b) => {
                 return new Date(a) - new Date(b);
             });
             console.log(uniqueDates);
-            // const today = new Date('May 22, 2023'); 
+
+            var routineDays = [];
+            var missingDays = [];
+            uniqueDates.forEach(date => {
+                // console.log(date);
+                let inRoutine = userRoutine.some(exercise => {
+                    return exercise.date === date;
+                });
+                if (inRoutine) {
+                    console.log('in routine');
+                    routineDays.push(date);
+                } else {
+                    missingDays.push(date);
+                }
+            });
+
+            if (userRoutine.some(exercise => {
+                console.log(exercise.exerciseName)
+                console.log(exercise.routineDay == null);
+                return exercise.routineDay == null;
+            })) {
+                console.log('adding routine days')
+                for (let i = 0; i < routineDays.length; i++) {
+                    let exercises = userRoutine.filter(exercise => {
+                        return exercise.date == routineDays[i];
+                    })
+
+                    for (let j = 0; j < exercises.length; j++) {
+                        console.log(exercises[j].exerciseName);
+                        await userCollection.findOneAndUpdate(
+                            { name: username, 'routine.exerciseName': exercises[j].exerciseName },
+                            {
+                                $set: {
+                                    'routine.$.routineDay': i
+                                }
+                            })
+                    }
+                }
+            }
+
+            console.log(missingDays);
+
+            if (missingDays.length > 0) {
+                console.log('in missing days');
+                var fillInExercises = [];
+                missingDays.forEach((day, idx) => {
+                    let fullDate = new Date(day);
+                    let routineDay = idx % routineDays.length;
+                    let exercises = userRoutine.filter(exercise => {
+                        return exercise.routineDay == routineDay;
+                    })
+                    exercises.forEach(routineExercise => {
+                        let exercise = { ...routineExercise };
+                        exercise.date = day;
+                        exercise.day = daysOfWeek[fullDate.getDay()];
+                        fillInExercises.push(exercise);
+                    })
+                });
+
+                // console.log(fillInExercises);
+
+                if (fillInExercises.length > 0) {
+                    console.log('days to fill');
+                    userRoutine = userRoutine.concat(fillInExercises);
+                    userCollection.findOneAndUpdate(
+                        { name: username },
+                        { $push: { routine: { $each: fillInExercises } } }
+                    )
+                }
+                // fillEmptyDays(username, fillInExercises);
+            }
+
+
+
+            // const today = new Date('May 24, 2023'); 
             var today = new Date(Date.now());
-            console.log(today.getTimezoneOffset());
-            today.setHours(today.getHours() - today.getTimezoneOffset() / 60);
-            console.log(today.getHours());
+            // today.setHours(today.getHours() - today.getTimezoneOffset() / 60);
             let cardContent;
             let dayCards = '';
 
             // For each unique day, create a card of exercises for that day.
-             uniqueDates.map(date => {
+            uniqueDates.map(date => {
                 var fullDate = new Date(date);
-                if (fullDate.getDate() < today.getDate()) {
+                if (fullDate < today && fullDate.getDate() < today.getDate()) {
                     return;
                 }
                 var day = '';
@@ -43,12 +120,12 @@ router.get('/', async (req, res) => {
                     if (day === '') {
                         day = exercise.day;
                     }
-                    
+
                     const formatExerciseName = exercise.exerciseName.slice(0, 1).toUpperCase() + exercise.exerciseName.slice(1).toLowerCase();
-                    const formatExerciseTarget = exercise.exerciseBodyPart.slice(0, 1).toUpperCase() 
+                    const formatExerciseTarget = exercise.exerciseBodyPart.slice(0, 1).toUpperCase()
                         + exercise.exerciseBodyPart.slice(1).toLowerCase()
                         + ' - '
-                        + exercise.exerciseTarget.slice(0, 1).toUpperCase() 
+                        + exercise.exerciseTarget.slice(0, 1).toUpperCase()
                         + exercise.exerciseTarget.slice(1).toLowerCase();
 
                     // Create a card and modal for the current exercise.
