e8e1a964b8779e80b63837dc9b7759fd6c43ec99
Mike Doswell <mike.doswell@gmail.com>
2023-05-24 13:56:48 -0700

    Updates home.js to populate feed by looping through days of initial exercise routine.

--- routes/home.js
+++ routes/home.js
@@ -17,157 +17,161 @@ router.get('/', async (req, res) => {
 
     if (req.session.authenticated) {
         if (userRoutine != null) {
-            const uniqueDates = Array.from(new Set(userAvailability.map(exercise => exercise.date))).sort((a, b) => {
-                return new Date(a) - new Date(b);
-            });
-            console.log(uniqueDates);
 
-            console.log('template null: ', routineTemplate == null);
-            if (routineTemplate == null) {
-                let newRoutineTemplate = [];
-                uniqueDates.forEach((date, idx) => {
-                    console.log(date);
-                    userRoutine.forEach(exercise => {
-                        if (exercise.date == date) {
-                            console.log(exercise.exerciseName)
-                            let templateExercise = { ...exercise };
-                            templateExercise.routineDay = idx;
-                            console.log(templateExercise.routineDay);
-                            newRoutineTemplate.push(templateExercise);
+
+            // Check that first time setup complete (routine days added)
+            if (userRoutine.some(exercise => {
+                return exercise.routineDay == null;
+            })) {
+                console.log('adding routine days')
+                var uniqueDates = [];
+                userRoutine.forEach(exercise => {
+                    if (!uniqueDates.includes(exercise.date)) {
+                        console.log(exercise.date);
+                        uniqueDates.push(exercise.date);
+                    }
+                });
+
+                uniqueDates = uniqueDates.sort((a, b) => {
+                    return new Date(a) - new Date(b);
+                });
+
+                console.log(uniqueDates);
+
+                let dayNum = 0;
+                uniqueDates.forEach(date => {
+                    userRoutine.forEach(async exercise => {
+                        if (exercise.date == date && exercise.routineDay == null) {
+                            console.log(exercise);
+                            exercise.routineDay = dayNum;
+                            await userCollection.findOneAndUpdate(
+                                { name: username, 'routine.exerciseName': exercise.exerciseName },
+                                {
+                                    $set: {
+                                        'routine.$.routineDay': dayNum
+                                    }
+                                })
                         }
-                    });
-                })
+                    })
+                    dayNum++;
+                });
 
+
+
+                // for (let i = 0; i < routineDays.length; i++) {
+                //     let exercises = userRoutine.filter(exercise => {
+                //         return exercise.date == routineDays[i];
+                //     })
+
+                //     for (let j = 0; j < exercises.length; j++) {
+                //         console.log(exercises[j].exerciseName);
+                //         await userCollection.findOneAndUpdate(
+                //             { name: username, 'routine.exerciseName': exercises[j].exerciseName },
+                //             {
+                //                 $set: {
+                //                     'routine.$.routineDay': i
+                //                 }
+                //             })
+                //     }
+                // }
+            }
+
+            // Check that workouts complete exists
+            var workoutsComplete = await userCollection.find({ name: { $eq: username } }).project({ workoutsComplete: 1 }).toArray();
+            workoutsComplete = workoutsComplete[0].workoutsComplete;
+            console.log('workoutsComplete: ', workoutsComplete);
+            if (workoutsComplete == null) {
+                console.log('is null');
+                workoutsComplete = 0;
                 await userCollection.findOneAndUpdate(
                     { name: username },
-                    { $set: { routineTemplate: newRoutineTemplate } }
+                    { $set: { workoutsComplete: workoutsComplete } }
                 )
             }
 
-            // var routineDays = [];
-            // var missingDays = [];
-            // var lastRoutineDate;
-            // var lastRoutineDay;
-            // uniqueDates.forEach(date => {
-            //     // console.log(date);
-            //     let inRoutine = userRoutine.some(exercise => {
-            //         return exercise.date === date;
-            //     });
-            //     if (inRoutine) {
-            //         console.log('in routine');
-            //         lastRoutineDate = date;
-            //         routineDays.push(date);
-            //     } else {
-            //         missingDays.push(date);
-            //     }
-            // });
-
-            // userRoutine.some(exercise => {
-            //     if (exercise.date == lastRoutineDate) {
-            //         lastRoutineDay = exercise.routineDay;
-            //         return true;
-            //     } else {
-            //         return false;
-            //     }
-            // })
-
-            // console.log('last date: ', lastRoutineDate);
-            // console.log('last day: ', lastRoutineDay);
-
-            // if (userRoutine.some(exercise => {
-            //     return exercise.routineDay == null;
-            // })) {
-            //     console.log('adding routine days')
-            //     for (let i = 0; i < routineDays.length; i++) {
-            //         let exercises = userRoutine.filter(exercise => {
-            //             return exercise.date == routineDays[i];
-            //         })
-
-            //         for (let j = 0; j < exercises.length; j++) {
-            //             console.log(exercises[j].exerciseName);
-            //             await userCollection.findOneAndUpdate(
-            //                 { name: username, 'routine.exerciseName': exercises[j].exerciseName },
-            //                 {
-            //                     $set: {
-            //                         'routine.$.routineDay': i
-            //                     }
-            //                 })
-            //         }
-            //     }
-            // }
-
-            // console.log(missingDays);
-
-            // if (missingDays.length > 0) {
-            //     console.log('missing length: ', missingDays.length);
-            //     var fillInExercises = [];
-            //     missingDays.forEach((day, idx) => {
-            //         let fullDate = new Date(day);
-            //         let uniqueRoutineDays = [];
-            //         userRoutine.forEach(exercise => {
-            //             if (!uniqueRoutineDays.includes(exercise.routineDay)) {
-            //                 uniqueRoutineDays.push(exercise.routineDay)
-            //             }
-            //         }) 
-            //         console.log('num routine days: ', uniqueRoutineDays.length)
-            //         let routineDay = (lastRoutineDay + 1 + idx) % uniqueRoutineDays.length;
-            //         console.log('next routine day: ', routineDay);
-            //         let includedNames = [];
-            //         let exercises = userRoutine.filter(exercise => {
-            //             if (exercise.routineDay == routineDay && !includedNames.includes(exercise.exerciseName)) {
-            //                 includedNames.push(exercise.exerciseName);
-            //                 return true;
-            //             } else {
-            //                 return false;
-            //             }
-            //         })
-            //         exercises.forEach(routineExercise => {
-            //             let exercise = { ...routineExercise };
-            //             console.log(exercise.exerciseName);
-            //             exercise.date = day;
-            //             exercise.day = daysOfWeek[fullDate.getDay()];
-            //             fillInExercises.push(exercise);
-            //         })
-            //     });
-
-            //     // console.log(fillInExercises);
-
-            //     if (fillInExercises.length > 0) {
-            //         console.log('days to fill');
-            //         console.log('fillin length: ', fillInExercises.length);
-            //         userRoutine = userRoutine.concat(fillInExercises);
-            //         userCollection.findOneAndUpdate(
-            //             { name: username },
-            //             { $push: { routine: { $each: fillInExercises } } }
-            //         )
-            //     }
-            // }
-
-
-
+            // Check for passed days and update workout complete
+            var availabilities = await getAvailability(username);
+            // console.log(availabilities);
             // const today = new Date('May 24, 2023'); 
             var today = new Date(Date.now());
-            // today.setHours(today.getHours() - today.getTimezoneOffset() / 60);
+            today.setHours(today.getHours() - today.getTimezoneOffset() / 60);
+            console.log(today);
+
+            var newAvailabilities = [];
+            availabilities.forEach(date => {
+                // console.log(date.date)
+                var fullDate = new Date(date.date);
+                console.log(fullDate.toDateString());
+                // console.log(fullDate >= today)
+                // console.log(fullDate.toDateString() == today.toDateString())
+                if (fullDate > today || fullDate.toDateString() == today.toDateString()) {
+                    console.log('in')
+                    newAvailabilities.push(date);
+                } else {
+                    console.log('passed')
+                    workoutsComplete++;
+                }
+            });
+            // console.log(newAvailabilities)
+            console.log('workoutsComplete:', workoutsComplete);
+            await userCollection.findOneAndUpdate(
+                { name: username },
+                { $set: { availabilityData: newAvailabilities, workoutsComplete: workoutsComplete } }
+            )
+            // console.log(test);
+
+
+
+            // Populate days in availability using routine days, starting from workoutsComplete % numRoutineDays
+            // var newAvail = await getAvailability(username);
+            // console.log(newAvail);
+
+            // const uniqueDates = Array.from(new Set(newAvail.map(avail => avail.date))).sort((a, b) => {
+            //     return new Date(a) - new Date(b);
+            // });
+            // console.log(uniqueDates);
+
+            var routineDays = [];
+            userRoutine.forEach(exercise => {
+                if (!routineDays.includes(exercise.routineDay)) {
+                    console.log('new routine day')
+                    routineDays.push(exercise.routineDay)
+                }
+            })
+
+            newAvailabilities = newAvailabilities.sort((a, b) => {
+                return new Date(a.date) - new Date(b.date);
+            });
+
             let cardContent;
             let dayCards = '';
+            var workoutNum = workoutsComplete;
+            var routineDay;
 
             // For each unique day, create a card of exercises for that day.
-             uniqueDays.map(day => {
-                var date = '';
+            newAvailabilities.forEach(availability => {
+                console.log(availability.date);
+                console.log('routine day:', workoutNum % routineDays.length)
+                routineDay = workoutNum % routineDays.length;
+                workoutNum++;
+
                 // Filter the user's routine for the current day.
-                const exercisesForDay = userRoutine.filter(exercise => exercise.date === date);
+                const exercisesForDay = userRoutine.filter(exercise => exercise.routineDay === routineDay);
+
+                if (exercisesForDay.length == 0) {
+                    return;
+                }
 
                 // For each exercise in the user's routine for the current day, create a card.
 
                 cardContent = exercisesForDay.map(exercise => {
+
                     // var exerciseDetails;
                     // exerciseDetails = await exerciseCollection.findOne(
                     //     { id: exercise.id },
                     //     { projection: { name: 1, bodyPart: 1, target: 1, gifUrl: 1, instructions: 1 } }).then(console.log(this));
                     // console.log('details: ' + exerciseDetails);
-                    if (day === '') {
-                        day = exercise.day;
-                    }
+
 
                     const formatExerciseName = exercise.exerciseName.slice(0, 1).toUpperCase() + exercise.exerciseName.slice(1).toLowerCase();
                     const formatExerciseTarget = exercise.exerciseBodyPart.slice(0, 1).toUpperCase()
@@ -187,7 +191,7 @@ router.get('/', async (req, res) => {
                                         More info
                                     </a>
                                     <div class='hideMenuButton'><span class='arrow'><span></span><span></span></span></div>
-                                    <div id="cardControls">
+                                    <div id="cardControls" class="pt-3">
                                         <button class="btn btn-primary replaceExerciseButton" data-exercise-name='${exercise.exerciseName}'>Replace</button>
                                         <button class="btn btn-danger deleteExerciseButton" data-exercise-name='${exercise.exerciseName}'>Delete</button>
                                     </div>
@@ -198,9 +202,9 @@ router.get('/', async (req, res) => {
 
                 // Join the cards for the current day into a single card. Add each day card to the dayCards String.
                 dayCards +=
-                    `<div class='day-card' id='${day}-card'>
-                        <h3 class='day-title' id='${day}-title'>${day}</h2>
-                        <h5 class='date-title' id='${day}-date-title'>${date}</h5>
+                    `<div class='day-card' id='${availability.dayOfWeek}-card'>
+                        <h3 class='day-title' id='${availability.dayOfWeek}-title'>${availability.dayOfWeek}</h2>
+                        <h5 class='date-title' id='${availability.dayOfWeek}-date-title'>${availability.date}</h5>
                         ${cardContent.join('')}
                     </div>`;
             });
